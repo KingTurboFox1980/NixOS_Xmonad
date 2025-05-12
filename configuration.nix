b908cfdb39ff5477ef2cd8f3c09161667462a08e
KingTurboFox1980 <kingturbofox@icloud.com>
2025-05-11 22:10:17 -0400

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -1,88 +1,98 @@
-{ config, lib, pkgs, ... }:   
+{ config, lib, pkgs, ... }:    
 
-let   
-  vivaldi = pkgs.vivaldi.overrideAttrs (oldAttrs: {   
-    dontWrapQtApps = false;   
-    dontPatchELF = true;   
-    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [pkgs.kdePackages.wrapQtAppsHook];   
-  });   
-in   
+let    
+  vivaldi = pkgs.vivaldi.overrideAttrs (oldAttrs: {    
+    dontWrapQtApps = false;    
+    dontPatchELF = true;    
+    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [pkgs.kdePackages.wrapQtAppsHook];    
+  });    
+in    
 
-{  
-  imports = [  
-    ./hardware-configuration.nix  
-  ];  
+{   
+  # Import Hardware Configuration  
+  imports = [ ./hardware-configuration.nix ];   
 
   # Bootloader Configuration  
-  boot.loader.systemd-boot.enable = true;  
-  boot.loader.efi.canTouchEfiVariables = true;  
+  boot.loader = {  
+    systemd-boot.enable = true;   
+    efi.canTouchEfiVariables = true;   
+  };  
 
-  # Networking  
-  networking.hostName = "nixos";  
-  networking.networkmanager.enable = true;  
+  # Networking Configuration  
+  networking = {  
+    hostName = "nixos";   
+    networkmanager.enable = true;   
+  };  
 
-  # Time and Locale Settings  
-  time.timeZone = "America/Toronto";  
-  i18n.defaultLocale = "en_CA.UTF-8";  
+  # Time & Locale Settings  
+  time.timeZone = "America/Toronto";   
+  i18n.defaultLocale = "en_CA.UTF-8";   
 
-  # Enable X server with XMonad 
-  services.xserver.enable = true; 
-  services.xserver.xkb.layout = "us";  
-  services.xserver.windowManager.xmonad.enable = true;
+  # X Server & Window Manager  
+  services.xserver = {  
+    enable = true;  
+    xkb.layout = "us";   
+    windowManager.xmonad.enable = true;   
+  };   
 
-  # Set Cursor Theme for X Server
-  environment.variables = {
-    XCURSOR_THEME = "Material-Black";
-    XCURSOR_SIZE = "24";
-    GTK_THEME = "Dracula";
-    GTK_ICON_THEME = "Papirus-Dark";
-  };
+  # Set Cursor & Theme Preferences  
+  environment.variables = {  
+    XCURSOR_THEME = "Material-Black";  
+    XCURSOR_SIZE = "24";  
+    GTK_THEME = "Dracula";  
+    GTK_ICON_THEME = "Papirus-Dark";  
+  };  
 
-  # Enable GNOME Keyring & Polkit
+  # Security & Authentication  
   services.gnome.gnome-keyring.enable = true;  
   security.polkit.enable = true;  
 
-  # User Account Configuration  
+  # User Configuration  
   users.users.j3ll0 = {  
     isNormalUser = true;  
     description = "Angelo";  
     extraGroups = [ "networkmanager" "wheel" "libvirtd" ];  
     packages = with pkgs; [ kdePackages.kate ];  
+  };   
+
+  # Virtualization Support  
+  programs.virt-manager.enable = true;  
+  users.groups.libvirtd.members = [ "j3ll0" ];  
+  virtualisation = {  
+    libvirtd.enable = true;  
+    spiceUSBRedirection.enable = true;   
+  };   
+
+  # System & Hardware Services  
+  services = {  
+    asusd.enable = true;      # ASUS-specific utilities  
+    udisks2.enable = true;    # USB mounting support  
+    gvfs.enable = true;       # GVFS for file system management  
   };  
 
-  # Enable virtualization  
-  virtualisation.libvirtd.enable = true;  
-  
-  # Asus 
-  services.asusd.enable = true;
-
-  # USB Mount
-  services.udisks2.enable = true;
-  
-  # GVFS
-  services.gvfs.enable = true;
-
-  # Performance Optimizations  
+  # Performance Optimization  
   zramSwap.enable = true;  
   programs.xfconf.enable = true;  
   boot.kernel.sysctl."vm.dirty_ratio" = 20;  
 
-  # Mount Additional Storage  
-  fileSystems."/mnt/downloads" = {  
-    device = "/dev/disk/by-uuid/4681ad39-ed76-4fe7-ab87-d3a03816a8a1";  
-    fsType = "ext4";  
-    options = [ "defaults" ];  
+  # Storage Configuration  
+  fileSystems = {  
+    "/mnt/downloads" = {  
+      device = "/dev/disk/by-uuid/4681ad39-ed76-4fe7-ab87-d3a03816a8a1";  
+      fsType = "ext4";  
+      options = [ "defaults" ];  
+    };  
+    "/tmp" = {  
+      device = "tmpfs";  
+      fsType = "tmpfs";  
+      options = [ "size=2G" "mode=1777" ];  
+    };  
   };  
 
-  fileSystems."/tmp" = {  
-    device = "tmpfs";  
-    fsType = "tmpfs";  
-    options = [ "size=2G" "mode=1777" ];  
-  };  
-
+  # Kernel Parameters  
   boot.kernelParams = [ "mitigations=off" ];  
- 
-  # Scheduled System Upgrades  
+
+  # System Update & Maintenance  
   system.autoUpgrade = {  
     enable = true;  
     flags = [ "--upgrade" "-L" ];  
@@ -91,147 +101,158 @@ in
     persistent = true;  
   };  
 
-  nix.settings.cores = 12;  
-  nix.gc.automatic = true;  
-  nix.gc.dates = "Sun *-*-* 05:00:00";  
+  nix = {  
+    settings.cores = 12;  
+    gc = {  
+      automatic = true;  
+      dates = "Sun *-*-* 05:00:00";  
+    };  
+  };  
 
   # Auto-Rebuild System After Updates  
-  systemd = {  
-    services.autoRebuild = {  
-      enable = true;  
-      description = "Auto-rebuild system after updates";  
-      serviceConfig = {  
-        ExecStart = "${pkgs.bash}/bin/bash -c 'nix-channel --update && nixos-rebuild switch'";  
-      };  
-    };  
-    timers.autoRebuild = {  
-      enable = true;  
-      description = "Trigger auto-rebuild system";  
-      timerConfig = {  
-        OnCalendar = "Sun *-*-* 05:00:00";  
-        Persistent = true;  
-      };  
-      wantedBy = [ "timers.target" ];  
-    };  
+  systemd.services.autoRebuild = {  
+    enable = true;  
+    description = "Auto-rebuild system after updates";  
+    serviceConfig.ExecStart = "${pkgs.bash}/bin/bash -c 'nix-channel --update && nixos-rebuild switch'";  
+  };  
+  systemd.timers.autoRebuild = {  
+    enable = true;  
+    timerConfig.OnCalendar = "Sun *-*-* 05:00:00";  
+    timerConfig.Persistent = true;  
   };  
 
   # Enable Unfree Packages  
   nix.settings.experimental-features = [ "nix-command" "flakes" ];  
   nixpkgs.config.allowUnfree = true;  
 
-  # Merge All Packages into One `environment.systemPackages`
-  environment.systemPackages = with pkgs; [ 
-    material-cursors
+  # Font Configuration  
+  fonts = {  
+    enableFontDir = true;  
+    enableGhostscriptFonts = true;  
+    packages = with pkgs; [  
+      corefonts vistafonts inconsolata terminus_font  
+      proggyfonts dejavu_fonts font-awesome ubuntu_font_family  
+      source-code-pro source-sans-pro source-serif-pro  
+      noto-fonts-emoji openmoji-color twemoji-color-font  
+    ];  
+  };  
 
-    # XMonad & Dependencies  
-    xmonad-with-packages 
-    haskellPackages.xmonad-contrib 
-    dmenu 
-    rofi 
-    xdotool
-    pkgs.haskellPackages.xmonad-contrib
-    pkgs.trayer
-    pkgs.xfce.xfce4-screenshooter
-    pkgs.xfce.xfce4-terminal
-    pkgs.dunst
-    pkgs.gtk3
-    python3
-    python3Packages.requests
-    python3Packages.configparser
-
-    # Browsers & Networking  
-    firefox 
-    microsoft-edge 
-    p3x-onenote 
-    vivaldi 
-    whatsapp-for-linux  
-    pkgs.networkmanager
-    pkgs.nm-tray
-
-    # Development Tools  
-    evolutionWithPlugins 
-    geany 
-    git 
-    neovim 
-    vim 
-    vscode  
-
-    # File Management  
-    pkgs.cifs-utils
-    samba
-    gvfs
-    pkgs.cifs-utils
-    kdePackages.dolphin 
-    rclone 
-    rclone-browser 
-    xfce.thunar 
-    xfce.thunar-archive-plugin 
-    xfce.thunar-volman  
-
-    # Multimedia & Theming  
-    alsa-utils 
-    brightnessctl 
-    pkgs.pywal 
-    vlc  
-    pkgs.xwallpaper
-    pkgs.volumeicon
-    pkgs.blueberry
-    
-    # UI Customization  
-    arandr 
-    arc-theme 
-    catppuccin-gtk
-    dracula-theme
-    gnome-tweaks 
+  # Merge All Packages into One `environment.systemPackages` 
+  environment.systemPackages = with pkgs; [  
     material-cursors 
-    pkgs.picom  
-    pkgs.redshift
+
+    # XMonad & Dependencies   
+    xmonad-with-packages  
+    haskellPackages.xmonad-contrib  
+    dmenu  
+    rofi  
+    xdotool 
+    pkgs.haskellPackages.xmonad-contrib 
+    pkgs.trayer 
+    pkgs.xfce.xfce4-screenshooter 
+    pkgs.xfce.xfce4-terminal 
+    pkgs.dunst 
+    pkgs.gtk3 
+    python3 
+    python3Packages.requests 
+    python3Packages.configparser 
+
+    # Browsers & Networking   
+    firefox  
+    microsoft-edge  
+    p3x-onenote  
+    vivaldi  
+    whatsapp-for-linux   
+    pkgs.networkmanager 
+    pkgs.nm-tray 
+ 
+    # Development Tools   
+    evolutionWithPlugins  
+    geany  
+    git  
+    neovim  
+    vim  
+    vscode   
+
+    # File Management   
+    samba 
+    gvfs 
+    kdePackages.dolphin  
+    rclone  
+    rclone-browser  
+    xfce.thunar  
+    xfce.thunar-archive-plugin  
+    xfce.thunar-volman   
+ 
+    # Multimedia & Theming   
+    alsa-utils  
+    brightnessctl  
+    pkgs.pywal  
+    vlc   
+    pkgs.xwallpaper 
+    pkgs.volumeicon 
+    pkgs.blueberry 
+    pkgs.nitrogen
+ 
+    # UI Customization   
+    arandr  
+    arc-theme  
+    catppuccin-gtk 
+    dracula-theme 
+    gnome-tweaks  
+    material-cursors  
+    pkgs.picom   
+    pkgs.redshift 
+    pkgs.lxappearance-gtk2
     
-    # Bars  
-    haskellPackages.xmobar 
-    pkgs.polybar  
+    # Bars   
+    haskellPackages.xmobar  
+    pkgs.polybar   
 
-    # System Tools & Virtualization  
-    pkgs.preload 
-    sxhkd 
-    pkgs.asusctl
-    bitwarden-desktop 
-    bleachbit 
-    gnome-extension-manager 
-    gnomeExtensions.open-bar 
-    gnomeExtensions.tray-icons-reloaded 
-    gnome-calendar 
-    gnome-disk-utility 
-    mate.engrampa 
-    networkmanager 
-    networkmanagerapplet 
-    polkit_gnome
-    pulseaudioFull 
-    swtpm 
-    sysstat 
-    virt-manager 
-    virtiofsd 
-    xclip 
-    pkgs.xfce.xfce4-power-manager
-    xorg.xev  
-        
-    # Terminal Utilities  
-    alacritty 
-    btop 
-    fastfetch 
-    fzf 
-    kitty 
-    starship 
-    zsh  
+    # System Tools & Virtualization   
+    pkgs.preload  
+    sxhkd  
+    pkgs.asusctl 
+    bitwarden-desktop  
+    bleachbit  
+    pkgs.conky
+    pkgs.lm_sensors
+    gnome-extension-manager  
+    gnomeExtensions.open-bar  
+    gnomeExtensions.tray-icons-reloaded  
+    gnome-calendar  
+    gnome-disk-utility  
+    mate.engrampa  
+    networkmanager  
+    networkmanagerapplet  
+    polkit_gnome 
+    pulseaudioFull  
+    swtpm  
+    sysstat  
+    virt-manager  
+    virtiofsd  
+    xclip  
+    pkgs.xfce.xfce4-power-manager 
+    xorg.xev   
 
-    # Other Utilities  
-    pkgs.copyq
-    qbittorrent 
-    vdhcoapp 
-    wget 
-    yt-dlp  
-  ];  
+    # Terminal Utilities   
+    alacritty  
+    btop  
+    fastfetch  
+    fzf  
+    kitty  
+    starship  
+    zsh   
 
-  # System State Version  
-  system.stateVersion = "24.11";  
+    # Other Utilities   
+    pkgs.copyq 
+    pkgs.mate.engrampa
+    qbittorrent  
+    vdhcoapp  
+    wget  
+    yt-dlp   
+  ];   
+
+  # System State Version   
+  system.stateVersion = "24.11";   
 }
--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,8 @@
 import System.IO
 import System.Exit
 import XMonad
+import Graphics.X11.ExtraTypes.XF86
+
 import XMonad.StackSet as W
 
 import XMonad.Config.Desktop
@@ -10,8 +12,10 @@ import XMonad.Hooks.SetWMName
 import XMonad.Hooks.DynamicLog (dynamicLogWithPP, wrap, xmobarPP, xmobarColor, shorten, PP(..))
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageHelpers (doFullFloat, doCenterFloat, isFullscreen, isDialog, doLower)
+import XMonad.Hooks.ManageHelpers (doFullFloat, doCenterFloat, doRectFloat, isFullscreen, isDialog, doLower)
 import XMonad.Hooks.UrgencyHook
+import XMonad.Hooks.EwmhDesktops (fullscreenEventHook)
+import XMonad.ManageHook
 
 import XMonad.Layout.Spacing
 import XMonad.Layout.Gaps
@@ -25,10 +29,15 @@ import XMonad.Layout.MultiToggle
 import XMonad.Layout.MultiToggle.Instances
 import XMonad.Layout.IndependentScreens
 import XMonad.Layout.CenteredMaster (centerMaster)
+import XMonad.Layout.Fullscreen (fullscreenEventHook, fullscreenManageHook, fullscreenSupport, fullscreenFull)
+import XMonad.Layout.ShowWName
 import XMonad.Layout.NoBorders (noBorders, smartBorders)
 
 import XMonad.Util.SpawnOnce
-import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.EZConfig  -- Simplifies keybindings
+import XMonad.Util.EZConfig (additionalKeys, additionalMouseBindings)
+import XMonad.Util.Hacks (windowedFullscreenFixEventHook, javaHack, trayerAboveXmobarEventHook, trayAbovePanelEventHook, trayerPaddingXmobarEventHook, trayPaddingXmobarEventHook, trayPaddingEventHook)
+import XMonad.Util.SpawnOnce
 import XMonad.Util.Run (spawnPipe)
 
 import Graphics.X11.ExtraTypes.XF86  -- Multimedia keys
@@ -44,21 +53,19 @@ fore     = "#BD93F9"
 back     = "#282A36"
 winType  = "#BD93F9"
 
-
 ---------------------------------------------------------------------------------
 -- Theme for showWName which prints current workspace when you change workspaces.
 ---------------------------------------------------------------------------------
 
 myShowWNameTheme :: SWNConfig
 myShowWNameTheme = def
-    { swn_font = "xft:Dina:size=75"
+    { swn_font = "xft:Crisp:size=75"
     , swn_fade    = 0.5
     , swn_bgcolor = "#1c1f24"
     , swn_color   = "#9580FF"
     }
 
 myModMask = mod4Mask
-encodeCChar = map fromIntegral
 myFocusFollowsMouse = True
 myBorderWidth = 2
 
@@ -66,48 +73,72 @@ myBorderWidth = 2
 -- Layouts --
 ------------------------------------------------------------------------
 
-myLayout = spacingRaw True (Border 5 5 5 5) True (Border 5 5 5 5) True $
+myLayout = spacingRaw True (Border 0 0 0 0) True (Border 5 5 5 5) True $
            smartBorders $
            avoidStruts $
            layoutHook def
+
+-------------------------------------------------------------------------
+-- Window Management Hook --
+------------------------------------------------------------------------
+
+myManageHook = composeAll . concat $
+    [ [isDialog --> doCenterFloat]  -- Center dialog boxes properly
+    , [className =? c --> doCenterFloat | c <- myCFloats]
+    , [title =? t --> doFloat | t <- myTFloats]
+    , [resource =? r --> doFloat | r <- myRFloats]
+    , [resource =? i --> doIgnore | i <- myIgnores]
+    , [className =? "vlc" --> doFloat]  -- Make VLC float
+    , [className =? "Conky" --> doIgnore]
+    , [title =? "system_conky" --> doIgnore]
+    , [isFullscreen --> doFullFloat]
+    , [manageDocks]
+    ]
+
+    where
+    myCFloats = ["Arandr", "Galculator", "feh", "mpv", "Xfce4-terminal"]
+    myTFloats = ["Downloads", "Save As...", "vlc"]
+    myRFloats = []
+    myIgnores = ["desktop_window", "Conky", "system_conky"]
 
 -------------------------------------------------------------------------
 -- Startup Hook --
 ------------------------------------------------------------------------
 
 myStartupHook = do
-    --spawnOnce "trayer --margin 2 --distancefrom left --distance 0 --edge top --align right --SetDockType false --SetPartialStrut true --expand true --width 10 --widthtype request --height 25 --heighttype pixel --transparent true --alpha 90 --padding 3 &"
     spawnOnce "picom --config ~/.config/picom/picom.conf &"
     spawnOnce "/home/j3ll0/.config/scripts/wallpaper.sh &"
     spawnOnce "nm-applet &"
     spawnOnce "copyq &"
     spawnOnce "blueberry-tray &"
     spawnOnce "redshift -P -l 43.8:-79.3 -O 4000 &"
-    spawnOnce "volumeicon &"
-    spawnOnce "polybar mainbar-xmonad &"
-    spawnOnce "polybar mainbar-xmonad-extra &"
+    spawnOnce "sleep 5 && volumeicon &"
+    spawnOnce "polybar mainbar-xmonad &" 
+    spawnOnce "polybar mainbar-xmonad-extra &" 
     spawnOnce "/home/j3ll0/.config/dunst/dunstrc &"
     spawnOnce "xsetroot -cursor_name left_ptr &"
     spawnOnce "/run/wrappers/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh"
     spawnOnce "/nix/store/75lv1npbf6zhsy1lk3v7z9indvwgm96d-polkit-gnome-0.105/libexec/polkit-gnome-authentication-agent-1 &"
+    spawnOnce "conky -c /home/j3ll0/.config/scripts/AUR-Allinone.conkyrc &"
 
 ------------------------------------------------------------------------
 -- Main Configuration --
 ------------------------------------------------------------------------
 
-main = xmonad $ ewmh $ docks def
-    { terminal    = "kitty"
-    , modMask     = mod4Mask  -- Use Super (Windows) key
-    , borderWidth = 3
-    , startupHook = myStartupHook
-    , layoutHook  = showWName' myShowWNameTheme $  -- Show workspace name when switching
-                    gaps [(U, 5), (D, 5), (R, 5), (L, 5)] $
-                    smartBorders $
-                    avoidStruts $
-                    myLayout
-    , focusedBorderColor = focdBord
-    , normalBorderColor = normBord
-    }
+main = xmonad $ ewmh $ docks def 
+    { terminal    = "kitty" 
+    , modMask     = mod4Mask  -- Use Super (Windows) key 
+    , borderWidth = 3 
+    , startupHook = myStartupHook 
+    , manageHook  = myManageHook
+    , layoutHook  = showWName' myShowWNameTheme $  -- Show workspace name when switching 
+                    gaps [(U, 5), (D, 5), (R, 5), (L, 5)] $ 
+                    smartBorders $ 
+                    avoidStruts $ 
+                    myLayout 
+    , focusedBorderColor = focdBord 
+    , normalBorderColor = normBord 
+    } 
 
         `additionalKeysP`
     [ -- Window Management
@@ -125,27 +156,32 @@ main = xmonad $ ewmh $ docks def
 
     -- Web Browsers
     , ("M-w", spawn "vivaldi")  -- Open Vivaldi browser
-    , ("M-S-w", spawn "microsoft-edge-stable")  -- Open Edge browser
+    , ("M-S-w", spawn "microsoft-edge")  -- Open Edge browser
 
     -- Programs
     , ("M-c", spawn "code")
-    , ("M-e", spawn "evolution")  -- Email client
-    , ("M-o", spawn "p3x-onenote")  -- OneNote
-    , ("M-r", spawn "rclone-browser")  -- Rclone Browser
-    , ("M-t", spawn "dolphin")  -- File manager
-    , ("M-v", spawn "virt-manager")  -- QEMU Virt-Manager
+    -- Evolution Email
+    , ("M-e", spawn "evolution")
+    -- OneNote
+    , ("M-o", spawn "p3x-onenote")
+    -- Rclone Browser
+    , ("M-r", spawn "rclone-browser")
+    -- Thunar
+    , ("M-t", spawn "thunar") 
+    -- QEMU Virt-Manager
+    , ("M-v", spawn "virt-manager") 
 
     -- Places
     , ("<XF86Launch1>", spawn "/home/j3ll0/.config/scripts/dwmfolders.sh")  -- Open places menu
 
+    -- Picom
+    , ("<KP_Multiply>", spawn "/home/j3ll0/.config/scripts/picom-toggle.sh")  -- Picom
+
     -- Pywal Wallpaper
     , ("<XF86AudioMicMute>", spawn "/home/j3ll0/.config/scripts/wallpaper.sh")  -- Pywal
 
-    -- Screen Off
-    , ("<F11>", spawn "xset dpms force off")  -- Screen OFF
-
     -- Power Menu
-    , ("M-x", spawn "/home/j3ll0/.config/scripts/power_rofi.sh")  -- Open power menu
+    --, ("M-x", spawn "/home/j3ll0/.config/scripts/power_rofi.sh")  -- Open power menu
 
     -- Recompile 
     , ("<KP_Add>", spawn "exec kitty -e /etc/nixos/nix_update.sh")  -- Recompile XMonad
@@ -154,7 +190,6 @@ main = xmonad $ ewmh $ docks def
     , ("M-<F9>", spawn "arandr")  -- Open screen management tool
     , ("<F9>", spawn "/home/j3ll0/.config/scripts/redshift.sh")
 
-    -- Brightness Control
     , ("<XF86DisplayOff>", spawn "/home/j3ll0/.config/scripts/brightOFF.sh")  -- Turn off brightness
     , ("<XF86MonBrightnessDown>", spawn "brightnessctl set 5%-")  -- Decrease brightness
     , ("<XF86MonBrightnessUp>", spawn "brightnessctl set +5%")  -- Increase brightness
@@ -163,6 +198,13 @@ main = xmonad $ ewmh $ docks def
     , ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")  -- Mute volume
     , ("<XF86AudioLowerVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ -10%")  -- Lower volume
     , ("<XF86AudioRaiseVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ +10%")  -- Raise volume
+
+    -- Key Hints
+    --, ("M-F5", spawn "/home/j3ll0/.config/polybar/scripts/keyhintdwm.sh")  -- Show key hints
+    --, ("M-S-F5", spawn "/home/j3ll0/.config/polybar/scripts/keyhintvim.sh")  -- Show Vim key hints
+
+    -- Display Power Management
+    , ("<F11>", spawn "xset dpms force off")  -- Turn off display
 
     -- Multimedia Keys
     , ("<XF86AudioPlay>", spawn "playerctl play-pause")  -- Play/Pause media
